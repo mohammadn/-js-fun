5c18b1a725df5cf8e22bb86751cb83cff202c0a0
mohammadn <mohammadnajafzade@yahoo.com>
2012-09-13 16:13:21 +0430

    edit script

--- najafzade.ir/script.js
+++ najafzade.ir/script.js
@@ -1,210 +1,259 @@
 $(function(){
 	
-	$('div.btn').mouseover(function(){
+	$('div.btn').mouseover( function(){
 		
 		$(this).animate({
-			'background-color':'rgba(200,200,200,0.8)'
+
+			'background-color' : 'rgba(200,200,200,0.8)'
+
 		},500,'ease-in-out');
 		
 	});
 	
-	$('div.btn1').mouseout(function(){
+	$('div.btn1').mouseout( function(){
 		
 		$(this).animate({
-			'background-color':'rgba(154,205,52,0.8)'
+
+			'background-color' : 'rgba(154,205,52,0.8)'
+
 		},500,'ease-in-out');
 		
 	});
 	
-	$('div.btn2').mouseout(function(){
+	$('div.btn2').mouseout( function(){
 		
 		$(this).animate({
-			'background-color':'rgba(0,96,45,0.8)'
+
+			'background-color' : 'rgba(0,96,45,0.8)'
+
 		},500,'ease-in-out');
 		
 	});
 	
-	$('div.btn4').mouseout(function(){
+	$('div.btn4').mouseout( function(){
 		
 		$(this).animate({
-			'background-color':'rgba(254,203,2,0.8)'
+
+			'background-color' : 'rgba(254,203,2,0.8)'
+
 		},500,'ease-in-out');
 		
 	});
 	
-	$('div.btn6').mouseout(function(){
+	$('div.btn6').mouseout( function(){
 		
 		$(this).animate({
-			'background-color':'rgba(0,200,252,0.8)'
+
+			'background-color' : 'rgba(0,200,252,0.8)'
+
 		},500,'ease-in-out');
 			
 	});
 	
-	$('div.btn7').mouseout(function(){
+	$('div.btn7').mouseout( function(){
 		
 		$(this).animate({
-			'background-color':'rgba(255,101,1,0.8)'
+
+			'background-color' : 'rgba(255,101,1,0.8)'
+
 		},500,'ease-in-out');
 		
 	});
 	
-	$('div.icon1').click(function(){
+	$('div.icon1').click( function(){
 		
 		$(this).animate({
-			'z-index':'1',
-			'background-color':'rgba(154,205,52,1)',
-			width:'1050px',
-			height:'695px',		
-			'background-position':'top right'
+			width                 : 1050,
+			height                : 695,	
+			'z-index'             : 1,
+			'background-color'    : 'rgba(154,205,52,1)',
+			'background-position' : 'top right'
 		},1000,'ease-in-out');
 		
 		$('div.icon1 p').animate({
-			'margin-top':'10px'
+
+			'margin-top' : 10
+
 		},2000,'ease-in-out');
 		
-	}).dblclick(function(){
+	}).dblclick( function(){
 		
 		$(this).animate({
-			width:'510px',
-			height:'280px',
-			'background-color':'rgba(154,205,52,0.8)',
-			'z-index':'0',
-			'background-position':'center center'
+
+			width                 : 510,
+			height                : 280,
+			'z-index'             : 0,
+			'background-color'    : 'rgba(154,205,52,0.8)',
+			'background-position' : 'center center'
+
 		},1000,'ease-in-out');
 		
 		$('div.icon1 p').animate({
-			'margin-top':'-40px'
+
+			'margin-top' : '-40px'
+
 		},500,'ease-in-out');
 		
 	});
 	
-	$('div.icon2').click(function(){
+	$('div.icon2').click( function(){
 		
 		$(this).animate({
-			'z-index':'1',
-			'background-color':'rgba(0,96,45,1)',
-			width:'1050px',
-			height:'695px',	
-			marginLeft:'0',
-			'background-position':'top right'
+
+			width                 : 1050,
+			height                : 695,
+			'z-index'             : 1,	
+			'margin-left'         : 0,
+			'background-color'    : 'rgba(0,96,45,1)',
+			'background-position' : 'top right'
+
 		},1000,'ease-in-out'
 		).removeClass('btn2 btn');
 		
 		$('div.icon2 p').animate({
-			'margin-top':'10px'
+
+			'margin-top' : 10
+
 		},2000,'ease-in-out');
 		
-	}).dblclick(function(){
+	}).dblclick( function(){
 		
 		$(this).animate({
-			width:'530px',
-			height:'150px',
-			marginLeft:'520px',
-			'background-color':'rgba(0,96,45,0.8)',
-			'z-index':'0',
-			'background-position':'center center'
+
+			width                 : 530,
+			height                : 150,
+			'margin-left'         : 520,
+			'z-index'             : 0,
+			'background-color'    : 'rgba(0,96,45,0.8)',
+			'background-position' : 'center center'
+
 		},1000,'ease-in-out'
 		).addClass('btn2 btn');
 		
 		$('div.icon2 p').animate({
-			'margin-top':'-40px'
+
+			'margin-top' : '-40px'
+
 		},500,'ease-in-out');
 		
 	});
-	$('div.icon4').click(function(){
+	$('div.icon4').click( function(){
 		
 		$(this).animate({
-			'z-index':'1',
-			'background-color':'rgba(254,203,2,1)',
-			width:'1050px',
-			height:'695px',	
-			marginLeft:0,
-			marginTop:0,	
-			'background-position':'top right'
+
+			width                 : 1050,
+			height                : 695,	
+			'z-index'             : 1,
+			'margin-left'         : 0,
+			'margin-top'          : 0,	
+			'background-color'    : 'rgba(254,203,2,1)',
+			'background-position' : 'top right'
+
 		},1000,'ease-in-out');
 		
 		$('div.icon4 p').animate({
-			'margin-top':'10px'
+
+			'margin-top' : 10
+
 		},2000,'ease-in-out');
 		
-	}).dblclick(function(){
+	}).dblclick( function(){
 		
 		$(this).animate({
-			width:'400px',
-			height:'310px',
-			'background-color':'rgba(254,203,2,0.8)',
-			'z-index':'0',
-			marginLeft:650,
-			marginTop:160,
-			'background-position':'center center'
+
+			width                 : 400,
+			height                : 310,
+			'z-index'             : 0,
+			'margin-left'         : 650,
+			'margin-top'          : 160,
+			'background-color'    : 'rgba(254,203,2,0.8)',
+			'background-position' : 'center center'
+
 		},1000,'ease-in-out');
 		
 		$('div.icon4 p').animate({
-			'margin-top':'-40px'
+
+			'margin-top' : '-40px'
+
 		},500,'ease-in-out');
 		
 	});
-	$('div.icon6').click(function(){
+	$('div.icon6').click( function(){
 		
 		$(this).animate({
-			'z-index':'1',
-			'background-color':'rgba(0,200,252,1)',
-			width:'1050px',
-			height:'695px',	
-			marginLeft:0,
-			marginTop:0,	
-			'background-position':'top right'
+			width                 : 1050,
+			height                : 695,
+			'z-index'             : 1,
+			'margin-left'         : 0,
+			'margin-top'          : 0,	
+			'background-color'    : 'rgba(0,200,252,1)',
+			'background-position' : 'top right'
+
 		},1000,'ease-in-out');
 		
 		$('div.icon6 p').animate({
-			'margin-top':'10px'
+
+			'margin-top' : 10
+
 		},2000,'ease-in-out');
 		
-	}).dblclick(function(){
+	}).dblclick( function(){
 		
 		$(this).animate({
-			width:'450px',
-			height:'180px',
-			'background-color':'rgba(0,200,252,0.8)',
-			marginLeft:190,
-			marginTop:290,
-			'z-index':'0',
-			'background-position':'center center'
+
+			width                 : 450,
+			height                : 180,
+			'margin-left '        : 190,
+			'margin-top'          : 290,
+			'z-index'             : 0,
+			'background-color'    : 'rgba(0,200,252,0.8)',
+			'background-position' : 'center center'
+
 		},1000,'ease-in-out');
 		
 		$('div.icon6 p').animate({
-			'margin-top':'-40px'
+
+			'margin-top' : '-40px'
+
 		},500,'ease-in-out');
 		
 	});
-	$('div.icon7').click(function(){
+	$('div.icon7').click( function(){
 		
 		$(this).animate({
-			width:'1050px',
-			height:'695px',
-			'z-index':'1',
-			'background-color':'rgba(255,101,1,1)',
-			marginTop:0,		
-			'background-position':'top right'
+
+			width                 : 1050,
+			height                : 695,
+			'z-index'             : 1,
+			'margin-top'          : 0,
+			'background-color'    : 'rgba(255,101,1,1)',		
+			'background-position' : 'top right'
+
 		},1000,'ease-in-out');
 		
 		$('div.icon7 p').animate({
-			'margin-top':'10px'
+
+			'margin-top' : 10
+
 		},2000,'ease-in-out');
 		
-	}).dblclick(function(){
+	}).dblclick( function(){
 		
 		$(this).animate({
-			width:'520px',
-			height:'210px',
-			'background-color':'rgba(255,101,1,0.8)',
-			'z-index':'0',
-			marginTop:480,
-			'background-position':'center center'
+
+			width                 : 520,
+			height                : 210,
+			'z-index'             : 0,
+			'margin-top'          : 480,
+			'background-color'    : 'rgba(255,101,1,0.8)',
+			'background-position' : 'center center'
+
 		},1000,'ease-in-out');
 		
 		$('div.icon7 p').animate({
-			'margin-top':'-40px'
+
+			'margin-top' : '-40px'
+
 		},500,'ease-in-out');
 	});
 
